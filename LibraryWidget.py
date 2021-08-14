7cafcfa3e50b40e0c7a57fba952caff655819ed6
Alexandre GIRIN <alexandre.girin@gmail.com>
2021-08-14 21:13:25 +0100

    Correct the problem on the offset now I need to integrate the second part of the Library

--- LibraryWidget.py
+++ LibraryWidget.py
@@ -20,14 +20,9 @@ class Library(QWidget):
 
         zoneconf1.append({"offset1": 0, "lengthled": 11})
         zoneconf1.append({"offset1": 11, "lengthled": 7})
-        zoneconf1.append({"offset1": 18, "lengthled": 10})
-        zoneconf1.append({"offset1": 28, "lengthled": 9})
-        zoneconf1.append({"offset1": 37, "lengthled": 16})
-        zoneconf1.append({"offset1": 110, "lengthled": 20})
-        zoneconf1.append({"offset1": 130, "lengthled": 10})
-        zoneconf1.append({"offset1": 140, "lengthled": 10})
-        zoneconf1.append({"offset1": 150, "lengthled": 10})
-        zoneconf1.append({"offset1": 160, "lengthled": 10})
+        zoneconf1.append({"offset1": 18, "lengthled": 11})
+        zoneconf1.append({"offset1": 29, "lengthled": 9})
+        zoneconf1.append({"offset1": 38, "lengthled": 16})
 
         # Create the specific push button for each Zone of the Library
 
@@ -37,7 +32,7 @@ class Library(QWidget):
             offset2 = offset2 + zone["lengthled"]
 
         for zone in zoneconf1:
-            self.Zones1.append(Zone(zone["pin"], zone["offset1"], zone["lengthled"], 0))
+            self.Zones1.append(Zone(zone["offset1"], zone["lengthled"], 0))
             self.Zones1[-1].setStyleSheet("QPushButton {background-color:black}")
             self.Zones1[-1].clickeddata.connect(self.DialogColorPicker.showFullScreen)
 
@@ -46,12 +41,10 @@ class Library(QWidget):
         i = -1
 
         for zone in zoneconf1:
+            print("Offset2 = ", offset2)
             self.Zones1[i].offset2 = offset2
             offset2 = offset2 + self.Zones1[i].get_ledlength()
             i = i - 1
-
-        for zone in self.Zones1:
-            print("Zone")
 
         # Create the interface
         i = 0
